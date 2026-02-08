79d035b3f089abc19f35228d5ca24aa4eb868195
Willian <williansfigueiredo@gmail.com>
2026-02-08 12:37:24 -0300

    Fix: corrigir caminho do index.html para public/index.html

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const upload = multer({ storage: storage });
 
 // --- SERVIR ARQUIVOS HTML NA RAIZ ---
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.get('/invoice', (req, res) => {
